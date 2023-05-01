13b0b0e8761ecadeac03ea26d11bc738ebd35278
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-05-01 10:56:28 +0100

    homepage cards added

---
+++ src/components/ProjectData.js
@@ -0,0 +1,80 @@
+export const projectData = [
+  {
+    id: 1,
+    title: "Weather App",
+    img: "https://www.knowledgehut.com/_next/image?url=https%3A%2F%2Fd2o2utebsixu4k.cloudfront.net%2Fmedia%2Fimages%2F1648455525726-Image-13.png&w=750&q=75",
+    description:
+      "A weather app that displays the current weather and a 5 day forecast for a city of your choice.",
+    skills_required: {
+      skill_1: "HTML",
+      skill_2: "CSS",
+      skill_3: "JavaScript",
+      skill_4: "OpenWeather API",
+    },
+  },
+  {
+    id: 2,
+    title: "Weather App",
+    img: "https://www.knowledgehut.com/_next/image?url=https%3A%2F%2Fd2o2utebsixu4k.cloudfront.net%2Fmedia%2Fimages%2F1648455525726-Image-13.png&w=750&q=75",
+    description:
+      "A weather app that displays the current weather and a 5 day forecast for a city of your choice.",
+    skills_required: {
+      skill_1: "HTML",
+      skill_2: "CSS",
+      skill_3: "JavaScript",
+      skill_4: "OpenWeather API",
+    },
+  },
+  {
+    id: 3,
+    title: "Weather App",
+    img: "https://www.knowledgehut.com/_next/image?url=https%3A%2F%2Fd2o2utebsixu4k.cloudfront.net%2Fmedia%2Fimages%2F1648455525726-Image-13.png&w=750&q=75",
+    description:
+      "A weather app that displays the current weather and a 5 day forecast for a city of your choice.",
+    skills_required: {
+      skill_1: "HTML",
+      skill_2: "CSS",
+      skill_3: "JavaScript",
+      skill_4: "OpenWeather API",
+    },
+  },
+  {
+    id: 4,
+    title: "Weather App",
+    img: "https://www.knowledgehut.com/_next/image?url=https%3A%2F%2Fd2o2utebsixu4k.cloudfront.net%2Fmedia%2Fimages%2F1648455525726-Image-13.png&w=750&q=75",
+    description:
+      "A weather app that displays the current weather and a 5 day forecast for a city of your choice.",
+    skills_required: {
+      skill_1: "HTML",
+      skill_2: "CSS",
+      skill_3: "JavaScript",
+      skill_4: "OpenWeather API",
+    },
+  },
+  {
+    id: 5,
+    title: "Weather App",
+    img: "https://www.knowledgehut.com/_next/image?url=https%3A%2F%2Fd2o2utebsixu4k.cloudfront.net%2Fmedia%2Fimages%2F1648455525726-Image-13.png&w=750&q=75",
+    description:
+      "A weather app that displays the current weather and a 5 day forecast for a city of your choice.",
+    skills_required: {
+      skill_1: "HTML",
+      skill_2: "CSS",
+      skill_3: "JavaScript",
+      skill_4: "OpenWeather API",
+    },
+  },
+  {
+    id: 6,
+    title: "Weather App",
+    img: "https://www.knowledgehut.com/_next/image?url=https%3A%2F%2Fd2o2utebsixu4k.cloudfront.net%2Fmedia%2Fimages%2F1648455525726-Image-13.png&w=750&q=75",
+    description:
+      "A weather app that displays the current weather and a 5 day forecast for a city of your choice.",
+    skills_required: {
+      skill_1: "HTML",
+      skill_2: "CSS",
+      skill_3: "JavaScript",
+      skill_4: "OpenWeather API",
+    },
+  },
+];
--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Sidebar.css'
+import { HomeOutlined } from '@mui/icons-material'
 const Sidebar = () => {
   return (
-    <div className='sidebarWrapper'>Sidebar</div>
+    <div className='sidebarWrapper'>
+      <div className='sidebarWrapper-contents'>
+      <div className='sidebarWrapper-content-1'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTND_EFKh9c47XeItrUh8GvR7TUaD1N66E3tw&usqp=CAU' alt='img' className='sidebarLogo'/>
+      </div>
+      </div>
+      <div className='sidebarWrapper-content-2'>
+        <HomeOutlined className='sidebarHome-icon' />
+      </div>
+    </div>
   )
 }
 
